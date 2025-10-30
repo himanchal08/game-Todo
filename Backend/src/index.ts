173a62b26a50e5acca088c2127289a4ac9c3158f
himanchal08 <khattrihimanchal17@gmail.com>
2025-10-30 15:57:18 +0530

    feat: initialize frontend project with Expo and TypeScript
    - Added package.json with essential dependencies for React Navigation, Supabase, and Expo.
    - Configured TypeScript with a base configuration from Expo and enabled strict mode.
    
    Completed Phase 0 : The setup

---
+++ Backend/src/index.ts
@@ -0,0 +1,27 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Health check
+app.get('/health', (req, res) => {
+  res.json({ status: 'OK', message: 'Your App is running!' });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
